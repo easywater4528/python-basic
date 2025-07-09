1a1a2208b68730449fc2c57ee43fbbeee719c238
easywater <easywater4528@example.com>
2025-07-09 23:21:14 +0900

    그래프 생성

---
+++ matplot_example.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# 간단한 데이터
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+# 그래프 그리기
+plt.plot(x, y, marker='o')
+plt.title("Sample Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.grid(True)
+plt.savefig("my_plot.png") 
+plt.show()
