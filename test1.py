21296011da88351f16c00a0a2d2e04fa2842abb8
easywater <easywater4528@gmail.com>
2025-07-26 01:19:10 +0900

    딕셔너리+리스트 중첩 문제풀이

--- test1.py
+++ test1.py
@@ -1,1 +1,19 @@
-"hello mynameisyoublue"
+character = {
+    "name" : "기사",
+    "level" : 12,
+    "items" : {
+        "sword" : "불꽃의 검",
+        "armor" : "풀플레이트"
+    },
+    "skill" : ["베기", "세게베기", "아주세게 베기"]
+}
+
+for key in character:
+    if type(character[key]) is dict:
+        for dic in character[key]:
+            print(dic, ":", character[key][dic])
+    elif type(character[key]) is list:
+        for lis in character[key]:
+            print(key, ":", lis)
+    else:
+        print(key, ":", character[key])
